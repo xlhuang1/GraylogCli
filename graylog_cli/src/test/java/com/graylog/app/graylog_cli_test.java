dfeddb1f851b0d6c0df58355d13fca8ebf5a99a6
Xin Lu Huang <xlhuang@gatech.edu>
2021-07-03 11:44:56 -0500

    build as maven project

---
+++ graylog_cli/src/test/java/com/graylog/app/graylog_cli_test.java
@@ -0,0 +1,20 @@
+package com.graylog.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class graylog_cli_test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
